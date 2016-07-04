d76370d2d4ef9192d939c79f1ebab3938abf0bd4
Jari Hanhela <jarihanhela@gmail.com>
2016-07-04 19:31:08 +0300

    Update to ES6

--- Reactive.js
+++ Reactive.js
@@ -1,15 +1,15 @@
-// This is a simple example showing how JavaScript could support reactive programming.
+// This is a simple example to demostrate how JavaScript could support reactive programming.
 
 createReactiveValue = function(initialValue) {
     return {
         "observers": [], // Call these functions with a new value when it has been set
-        "value":initialValue,
+        "value": initialValue,
         "set": function(newValue) {
             this.value = newValue;
             
-            this.observers.forEach(function(observer) {
+            this.observers.forEach((observer) => {
                 // Call observers asynchronously
-                setTimeout(function() { observer(this.value) }, 0);
+                setTimeout(() => { observer(this.value) }, 0);
             })
         },
         "get": function() {
@@ -23,8 +23,8 @@ createReactiveValue = function(initialValue) {
 
 // If any of the dependant values change, sets reactive value 'value' to the result of calling 'result'
 makeReaction = function(value, dependencies, result) {
-    dependencies.forEach(function(dependency) {
-        dependency.addObserver(function(newValue) {
+    dependencies.forEach((dependency) => {
+        dependency.addObserver((newValue) => {
             var newResult = result(value, dependencies);
             value.set(newResult);
         });
@@ -34,7 +34,7 @@ makeReaction = function(value, dependencies, result) {
 
 // When 'reactiveValue' is changed, calls the given 'result' function with the new value
 watchReaction = function(reactiveValue, result) {
-    reactiveValue.addObserver(function(newValue) {
+    reactiveValue.addObserver((newValue) => {
         result(newValue);
     });
 }
@@ -44,27 +44,25 @@ var b = createReactiveValue(1);
 var c = createReactiveValue(null);
 
 // We want c to depend on both a and b. If either a or b change, c should automatically recalculate it's value
-makeReaction(c, [a, b], function(c, [a, b]) {
+makeReaction(c, [a, b], (c, [a, b]) => {
     return a.get() + b.get();
 })
 
 // If c is changed, for whatever reason, log it
-watchReaction(c, function(newC) {
+watchReaction(c, (newC) => {
     console.log("Reaction completed: " + a.get() + " + " + b.get() + " = " + c.get());
 });
 
 // Test it by resetting a and b randomly
 
-setInterval(function()
+setInterval(() =>
 {
     var newValue = Math.random();
     a.set(newValue); },
 1000);
 
-setInterval(function()
+setInterval(() =>
 {
     var newValue = Math.random();
     b.set(newValue); },
 1300);
-
-
